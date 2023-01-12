caff4017bcaf376d86c4093ca3914cd2cc40e68a
Jakub Łągwa <kklagwa@gmail.com>
2023-01-12 20:58:02 +0100

    another one

--- src/Game/GamePanel.java
+++ src/Game/GamePanel.java
@@ -3,11 +3,24 @@ package Game;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 	
 	static final int SCREEN_WIDTH = 1300;
 	static final int SCREEN_HEIGHT = 750;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = 
+			(SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
+	static final int DELAY = 100;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	boolean running = false;
+	int playerX;
+	int playerY;
+	char direction = 'R';
+	Timer timer;
+	Random random;
 	
 	GamePanel(){
 		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -18,17 +31,45 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	
 	public void startGame() {
-		
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
 	}
 	
 	public void paintComponent(Graphics g) {
-		
+		super.paintComponent(g);
+		draw(g);
 	}
 	public void draw(Graphics g) {
+		if(running) {
+			g.setColor(Color.red);
+			g.fillOval(playerX, playerY, UNIT_SIZE, UNIT_SIZE);
+			
+			
+		}
+		else {
+			gameOver(g);
+		}
+	}
+	
+	public void move() {
+		switch(direction) {
+		case 'W':
+			
+		case 'S':
+			
+		case 'A':
+			
+		case 'D':
+			
+		}
+	}
+	
+	public void checkCollisions() {
 		
 	}
 	
-	public void gameOver() {
+	public void gameOver(Graphics g) {
 		
 	}
 
